3e127330b9af90bcd584687595d615630c7a9879
matissbby <matissrolavs2005@gmail.com>
2025-02-27 11:20:57 +0200

    123

---
+++ src/components/question3.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Questions.css";
+const Question3 = () => {
+  return (
+    <div className="question">
+      <p className="question-text">kādi UML elementi ir šajā diagrammā?</p>
+      <p className="question-text">answers</p>
+    </div>
+  );
+};
+
+export default Question3;
