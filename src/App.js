271f4543a175929e5a3eea7549a9c34f4ad936e9
matissbby <matissrolavs2005@gmail.com>
2025-02-27 11:25:24 +0200

    sddsds
    dsdssd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Question3 from "./components/Question3";
 
 function App() {
-  return <div>yjtukrherhtf</div>;
+  return (
+    <article style={{ height: "100vh", overflowY: "scroll" }}>
+      <Question3 />
+    </article>
+  );
 }
 
 export default App;
