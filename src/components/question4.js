bcd8f2b1a5810fff57d6fc6a06922f4599fc245e
matissbby <matissrolavs2005@gmail.com>
2025-03-03 13:37:05 +0200

    aaa
    aaa

--- src/components/question4.js
+++ src/components/question4.js
@@ -1,26 +1,14 @@
 import React from "react";
 import "./Questions.css";
-import "/image.png";
+import image from "./image.png";
 const Question4 = () => {
   return (
-<<<<<<< Updated upstream
-    <article
-      style={{
-        height: "100vh",
-        overflowY: "scroll",
-      }}
-    >
-      <div className="question2">
-        <p className="question-text">kāds ir piemērs ar skaidrojumu?</p>
-        <p className="question-text">Ja</p>
-      </div>
-    </article>
-=======
     <div className="question">
       <p className="question-text">kāds ir piemērs ar skaidrojumu?</p>
-      <p className="question-text"></p>
+      <p className="question-text">computer - main klase</p>
+      <p className="question-text">mainboard, cpu, memory - atributi</p>
+      <img src={image} alt="diagramma" />
     </div>
->>>>>>> Stashed changes
   );
 };
 
