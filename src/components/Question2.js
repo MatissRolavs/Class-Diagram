8f356e4cb702ad4d2aa2385c948f34d2100df13c
Dripsters123 <129275287+Dripsters123@users.noreply.github.com>
2025-03-03 13:21:23 +0200

    yah

--- src/components/Question2.js
+++ src/components/Question2.js
@@ -5,14 +5,8 @@ const Question2 = () => {
 
     <div className="question">
       <p className="question-text">Kad tiek izmantotas klašu diagrammas?</p>
-      <p className="question-text">Klašu diagrammas tiek izmantotas, lai attēlotu dažādas klases un to savstarpējās attiecības, īpašības un metodes. Tās bieži izmanto, lai plānotu programmatūras arhitektūru, dokumentētu sistēmu un noteiktu objektu attiecības.</p>
-      <p className="question-text">Tās parasti izmanto, lai:</p>
-      <ul className="question-text">
-          <li>Plānotu programmatūras arhitektūru: Klašu diagrammas palīdz izstrādātājiem saprast un plānot, kā dažādas klases un objekti mijiedarbosies savā starpā.</li>
-          <li>Dokumentētu sistēmu: Klašu diagrammas var kalpot kā dokumentācija, kas sniedz pārskatu par sistēmas struktūru un loģiku.</li>
-          <li>Veidotu objektu attiecības: Tās palīdz noteikt, kā dažādi objekti būs saistīti un kā tie savstarpēji mijiedarbosies, veidojot sistēmu.</li>
-      </ul>
-
+      <p className="question-text">Klašu diagrammas tiek izmantotas, lai attēlotu dažādas klases un to savstarpējās attiecības, īpašības un metodes.<br /> Tās bieži izmanto, lai plānotu programmatūras arhitektūru.</p>
+     
     </div>
   );
 };
