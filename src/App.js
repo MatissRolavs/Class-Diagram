e7ec3572319b2835f5d9e68a99fd518ff4900cf2
Dripsters123 <129275287+Dripsters123@users.noreply.github.com>
2025-03-03 12:59:05 +0200

    Check it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import Question2 from "./components/Question2";
 import Question3 from "./components/Question3";
 
 function App() {
   return (
-    <article style={{ height: "100vh", overflowY: "scroll" }}>
-      <Question3 />
-    </article>
+    <>
+      <article style={{ height: "100vh", overflowY: "scroll" }}>
+        <Question2 />
+      </article>
+      <article style={{ height: "100vh", overflowY: "scroll" }}>
+        <Question3 />
+      </article>
+    </>
   );
 }
 
---
+++ src/components/Question2.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Question2 = () => {
+  return (
+    <div className="question">
+      <p className="question-text">Kad to izmanto?</p>
+      <p className="question-text">Tās izmanto, lai attēlotu dažādas klases un to savstarpējās attiecības, īpašības un metodes.</p>
+    </div>
+  );
+};
+
+export default Question2;
