042453368c58822cd0a302d58ee3b7db9f0aa221
OskarsEngelss <126758362+OskarsEngelss@users.noreply.github.com>
2025-03-03 13:19:27 +0200

    Create Question1.js

---
+++ src/components/Question1.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Questions.css";
+
+const Question1 = () => {
+    return (
+        <article>
+            <div>
+                <h2>Kas ir klases diagramma?</h2>
+                <p>Tā ir diagramma, kura attēlo klašu informāciju pārskatāmā veidā.</p>
+            </div>
+            <div>
+                <h2>Struktūras vai uzvedības modeļa daļa?</h2>
+                <p>Klases diagrammas ir struktūras modeļa daļa - attēlo klases, to īpašības un savstarpējās attiecības.</p>
+            </div>
+        </article>
+      );
+}
+
+export default Question1;
