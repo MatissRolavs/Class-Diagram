bc6feee51fe9d829455a3512b4d409918fb4754d
railijs <railijsgrieznis@gmail.com>
2025-03-03 12:44:39 +0200

    q4
    a

---
+++ src/components/question4.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Questions.css";
+const Question4 = () => {
+  return (
+    <div className="question">
+      <p className="question-text">kāds ir piemērs ar skaidrojumu?</p>
+      <p className="question-text">Ja</p>
+    </div>
+  );
+};
+
+export default Question4;
